12553caed819a505915a1e45a9f89b51a56ad0bc
Arpan Dey <arpandey.web@gmail.com>
2026-01-20 14:14:04 +0600

    implemented loading and error handler within about route also implemented theme toggling using shadcn

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,10 @@
-import React from 'react'
 
-export default function About() {
+export default async function About() {
+  await new Promise((resolve) => setTimeout(resolve, 4000));
+  throw new Error("something went wrong")
   return (
-    <div>This is about page</div>
-  )
+    <div>
+      <h1>This is about page</h1>
+    </div>
+  );
 }
