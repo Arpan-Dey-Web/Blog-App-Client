5d2a7d8b4e87ba012f3d2473717ae9de02fef60a
Arpan Dey <arpandey.web@gmail.com>
2026-01-26 14:48:41 +0600

    implemented user login and registration features

--- src/components/modules/auth/Register-form.tsx
+++ src/components/modules/auth/Register-form.tsx
@@ -20,6 +20,8 @@ import { Input } from "../../ui/input";
 
 import { useForm } from "@tanstack/react-form";
 import * as z from "zod";
+import { authClient } from "@/src/lib/auth-client";
+import { toast } from "sonner";
 
 const formSchema = z.object({
   name: z.string().min(1, "This field is required"),
@@ -37,11 +39,29 @@ export function RegisterForm({ ...props }: React.ComponentProps<typeof Card>) {
     validators: {
       onSubmit: formSchema,
     },
+
     onSubmit: async ({ value }) => {
-      console.log(value);
+      const toastId = toast.loading("Creating User");
+      try {
+        const { data, error } = await authClient.signUp.email(value);
+        if (error) {
+          toast.error(error.message, { id: toastId });
+        return
+        }
+        toast.success("User Created Sucessfully", {id : toastId})
+      } catch (error) {
+        // console.log(error);
+        toast.error("Something Went Wrong, Please try again ,", {id:toastId})
+      }
     },
   });
-
+ const handleGoogleLogin = async () => {
+   const data = authClient.signIn.social({
+     provider: "google",
+     callbackURL: "http://localhost:3000",
+   });
+   console.log(data);
+ };
   return (
     <Card {...props}>
       <CardHeader>
@@ -125,9 +145,17 @@ export function RegisterForm({ ...props }: React.ComponentProps<typeof Card>) {
           </FieldGroup>
         </form>
       </CardContent>
-      <CardFooter className="flex justify-end">
-        <Button form="register-form" type="submit">
-          Submit
+      <CardFooter className="flex flex-col  gap-3">
+        <Button form="register-form" type="submit" className="w-full ">
+         Register Your Account
+        </Button>
+        <Button
+          onClick={() => handleGoogleLogin()}
+          variant="outline"
+          type="button"
+          className="w-full"
+        >
+          Login with Google
         </Button>
       </CardFooter>
     </Card>
